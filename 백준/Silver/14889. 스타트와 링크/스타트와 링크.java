8d9dd3fe9537ca8b1a9ead333418224d2165ab0f
DevSeoRex <ckdekrn88@gmail.com>
2023-11-19 18:30:22 +0900

    [Silver I] Title: 스타트와 링크, Time: 372 ms, Memory: 21200 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,93 +1,84 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+package onlineJudge.backjun;
+
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	static int N;
-	static int[][] map;
-	static boolean[] visit;
-	
-	static int Min = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		N = Integer.parseInt(br.readLine());
- 
-		map = new int[N][N];
-		visit = new boolean[N];
- 
- 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
- 
-		combi(0, 0);
-		System.out.println(Min);
- 
-	}
- 
-	// idx는 인덱스, count는 조합 개수(=재귀 깊이)
-	static void combi(int idx, int count) {
-		// 팀 조합이 완성될 경우
-		if(count == N / 2) {
-			/*
-			 방문한 팀과 방문하지 않은 팀을 각각 나누어
-			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
-			*/
-			diff();
-			return;
-		}
- 
-		for(int i = idx; i < N; i++) {
-			// 방문하지 않았다면?
-			if(!visit[i]) {
-				visit[i] = true;	// 방문으로 변경
-				combi(i + 1, count + 1);	// 재귀 호출
-				visit[i] = false;	// 재귀가 끝나면 비방문으로 변경
-			}
-		}
-	}
- 
-	// 두 팀의 능력치 차이를 계산하는 함수 
-	static void diff() {
-		int team_start = 0;
-		int team_link = 0;
- 
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				// i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스 
-				if (visit[i] == true && visit[j] == true) {
-					team_start += map[i][j];
-					team_start += map[j][i];
-				}
-				// i 번째 사람과 j 번째 사람이 false라면 링크팀으로 점수 플러스 
-				else if (visit[i] == false && visit[j] == false) {
-					team_link += map[i][j];
-					team_link += map[j][i];
-				}
-			}
-		}
-		// 두 팀의 점수 차이 (절댓값)
-		int val = Math.abs(team_start - team_link);
-		
-		/*
-		  두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
-		  더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
-		 */
-		if (val == 0) {
-			System.out.println(val);
-			System.exit(0);
-		}
-		
-		Min = Math.min(val, Min);
-				
-	}
- 
+
+    static int N;
+    static int ANSWER = Integer.MAX_VALUE;
+    static int[][] teamValue;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        DFS(0,0);
+        output();
+    }
+
+    static void DFS(int idx, int L) throws IOException {
+        if (N / 2 == L) {
+            checkValue();
+            return;
+        }
+
+        for (int i=idx; i<N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                DFS(i + 1, L + 1);
+                visit[i] =false;
+            }
+        }
+    }
+
+    static void checkValue() throws IOException {
+        int start = 0;
+        int link = 0;
+
+        for (int i=0; i<N-1; i++) {
+            for (int j=i+1; j<N; j++) {
+                if (visit[i] && visit[j]) {
+                    start += teamValue[i][j];
+                    start += teamValue[j][i];
+                } else if (!visit[i] && !visit[j]) {
+                    link += teamValue[i][j];
+                    link += teamValue[j][i];
+                }
+            }
+        }
+
+        ANSWER = Math.min(ANSWER, Math.abs(start - link));
+
+        if (ANSWER == 0) {
+            output();
+            System.exit(0);
+        }
+    }
+
+
+
+
+    static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(ANSWER));
+        bw.flush();
+        bw.close();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        teamValue = new int[N][N];
+        visit = new boolean[N];
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++) {
+                teamValue[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
+        }
+
+        br.close();
+    }
 }
